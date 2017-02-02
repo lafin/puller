0afe3a11364cb3035ddbeaea36bf13cce1c7cec3
Kuvakin Sergey <kuvakin@gmail.com>
2017-02-03 00:16:25 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+		All: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Println(container.Image, container.Command, container.ID)
+	}
+
+	fmt.Println("done")
+}
