813c2f53375f141c41dce71b0eaf8bca743389c0
Kuvakin Sergey <kuvakin@gmail.com>
2017-02-13 00:13:49 +0300

    :walking:

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"io"
 	"os"
@@ -23,15 +24,44 @@ func main() {
 	}
 
 	for _, container := range containers {
-		fmt.Println(container.Image, container.Command, container.ID)
-	}
+		reader, err := cli.ImagePull(context.Background(), container.Image, types.ImagePullOptions{})
+		if err != nil {
+			panic(err)
+		}
 
-	reader, err := cli.ImagePull(context.Background(), "alpine", types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
-	}
+		io.Copy(os.Stdout, reader)
 
-	io.Copy(os.Stdout, reader)
+		config, err := cli.ContainerInspect(context.Background(), container.ID)
+		if err != nil {
+			panic(err)
+		}
+
+		isRunning := container.State == "running"
+
+		networkConfig := network.NetworkingConfig{
+			EndpointsConfig: config.NetworkSettings.Networks,
+		}
+
+		err = cli.ContainerRemove(context.Background(), container.ID, types.ContainerRemoveOptions{
+			Force: true,
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		container, err := cli.ContainerCreate(context.Background(), config.Config, config.HostConfig, &networkConfig, config.Name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(container)
+
+		if isRunning {
+			err = cli.ContainerStart(context.Background(), container.ID, types.ContainerStartOptions{})
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
 
 	fmt.Println("done")
 }
