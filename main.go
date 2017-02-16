3a67f65437d871061ef9fae710075fdb7b46ea24
Kuvakin Sergey <kuvakin@gmail.com>
2017-02-17 00:50:47 +0300

    :walking:

--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
-	"io"
-	"os"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
+	policyUpdating := flag.String("policy", "all", "policy of updateting container (all, running)")
+	// autoUpdating := flag.Bool("auto", false, "auto update by schedule (checking every 10 min)")
+	// webUI := flag.Bool("web", false, "run web ui")
+	// hookUpdating := flag.Bool("hook", false, "update by hook")
+	// notify := flag.Bool("notify", false, "notify about fail")
+	// rollback := flag.Bool("rollback", false, "rollback after fail (only for containers which was runned)")
+	// internalNetwork := flag.Bool("network", false, "puller's network")
+
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
@@ -24,20 +33,28 @@ func main() {
 	}
 
 	for _, container := range containers {
+		wasRunned := container.State == "running"
+		if *policyUpdating == "running" && !wasRunned {
+			continue
+		}
+
 		reader, err := cli.ImagePull(context.Background(), container.Image, types.ImagePullOptions{})
 		if err != nil {
 			panic(err)
 		}
 
-		io.Copy(os.Stdout, reader)
+		output, err := ioutil.ReadAll(reader)
+		if err != nil {
+			panic(err)
+		}
+		if strings.Contains(string(output), "Image is up to date") {
+			continue
+		}
 
 		config, err := cli.ContainerInspect(context.Background(), container.ID)
 		if err != nil {
 			panic(err)
 		}
-
-		isRunning := container.State == "running"
-
 		networkConfig := network.NetworkingConfig{
 			EndpointsConfig: config.NetworkSettings.Networks,
 		}
@@ -53,9 +70,8 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(container)
 
-		if isRunning {
+		if wasRunned {
 			err = cli.ContainerStart(context.Background(), container.ID, types.ContainerStartOptions{})
 			if err != nil {
 				panic(err)
